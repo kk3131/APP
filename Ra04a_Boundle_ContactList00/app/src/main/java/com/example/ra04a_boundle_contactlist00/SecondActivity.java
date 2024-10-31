bd6c437c5807828730f5f7991afb4d087b617aef
kk3131 <liuf979@gmail.com>
2024-10-31 13:52:50 +0800

    實作完兩個Activity，part1完成

--- Ra04a_Boundle_ContactList00/app/src/main/java/com/example/ra04a_boundle_contactlist00/SecondActivity.java
+++ Ra04a_Boundle_ContactList00/app/src/main/java/com/example/ra04a_boundle_contactlist00/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.ra04a_boundle_contactlist00;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,24 @@ public class SecondActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Bundle bundle=this.getIntent().getExtras();
+        String Name=bundle.getString("Name");
+        String Tel=bundle.getString("Tel");
+        String Phone=bundle.getString("Phone");
+        String Email=bundle.getString("Email");
+
+        TextView tv1=(TextView)findViewById(R.id.textView8);
+        TextView tv2=(TextView)findViewById(R.id.textView9);
+        TextView tv3=(TextView)findViewById(R.id.textView10);
+        TextView tv4=(TextView)findViewById(R.id.textView11);
+
+        tv1.setText("姓名: "+Name);
+        tv2.setText("電話: "+Tel);
+        tv3.setText("手機: "+Phone);
+        tv4.setText("Email: "+Email);
+
+
 
     }
 }
